638a8f46da0a5f3da51a1327884121da654bcc35
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2022-08-01 21:43:27 +0530

    fix: debugged error

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ let btnTranslate = document.querySelector("#btn-translate");
 let clearBtn = document.querySelector("#clr-btn");
 let userInput = document.querySelector("#user-input");
 let output = document.querySelector("#output");
-// let url = "https://api.funtranslations.com/translate/shakespeare.json";
-let url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+let url = "https://api.funtranslations.com/translate/shakespeare.json";
+// let url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
 btnTranslate.addEventListener("click", clickEventHandler);
 
@@ -20,7 +20,6 @@ function clickEventHandler() {
     .catch(errorHandler);
 }
 function errorHandler(error) {
-  console.log("error" + error);
   alert("something wrong with the servor please try again later");
   userInput.value = "";
 }
