d0460a9a5c50aeaaa2362ab7f96204f6c17cf7ef
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2022-08-01 16:35:46 +0530

    added error handler and clear btn

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 let btnTranslate = document.querySelector("#btn-translate");
+let clearBtn = document.querySelector("#clr-btn");
 let userInput = document.querySelector("#user-input");
 let output = document.querySelector("#output");
+let url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
 btnTranslate.addEventListener("click", clickEventHandler);
 
+clearBtn.addEventListener("click", function () {
+  userInput.value = "";
+  output.innerText = "";
+});
+
 function clickEventHandler() {
-  output.innerText = userInput.value;
+  let text = url + "?text=" + userInput.value;
+  fetch(text)
+    .then((response) => response.json())
+    .then((json) => (output.innerText = json.contents.translated))
+    .catch(errorHandler);
+}
+function errorHandler(error) {
+  alert("something wrong with the servor please try again later");
 }
