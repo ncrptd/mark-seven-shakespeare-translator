4e7be1a6d02d373b7b4c31c4777d21279a8fc1c5
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2022-08-01 15:23:18 +0530

    added output div

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let btnTranslate = document.querySelector("#btn-translate");
 let userInput = document.querySelector("#user-input");
+let output = document.querySelector("#output");
 
 btnTranslate.addEventListener("click", clickEventHandler);
 
 function clickEventHandler() {
-  console.log(userInput.value);
+  output.innerText = userInput.value;
 }
