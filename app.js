e0dabfa7c99e4d7c1081f9f417801a29f7c2ce58
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2022-08-01 21:53:03 +0530

    fixed empty text field

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ let btnTranslate = document.querySelector("#btn-translate");
 let clearBtn = document.querySelector("#clr-btn");
 let userInput = document.querySelector("#user-input");
 let output = document.querySelector("#output");
-let url = "https://api.funtranslations.com/translate/shakespeare.json";
-// let url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// let url = "https://api.funtranslations.com/translate/shakespeare.json";
+let url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
 btnTranslate.addEventListener("click", clickEventHandler);
 
@@ -13,11 +13,15 @@ clearBtn.addEventListener("click", function () {
 });
 
 function clickEventHandler() {
-  let text = url + "?text=" + userInput.value;
-  fetch(text)
-    .then((response) => response.json())
-    .then((json) => (output.innerText = json.contents.translated))
-    .catch(errorHandler);
+  if (userInput.value !== "") {
+    let text = url + "?text=" + userInput.value;
+    fetch(text)
+      .then((response) => response.json())
+      .then((json) => (output.innerText = json.contents.translated))
+      .catch(errorHandler);
+  } else {
+    output.innerText = "You forgot to write";
+  }
 }
 function errorHandler(error) {
   alert("something wrong with the server please try again later");
